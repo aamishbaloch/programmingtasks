a27f74bc038fc52b2c7c69509c09424e439eeded
Aamish Baloch <aamish.baloch@gmail.com>
2017-09-11 12:39:26 +0500

    merge sort implementation

---
+++ ArrayRelated/merge_sort.py
@@ -0,0 +1,41 @@
+def mergesort(numbers):
+    if len(numbers) > 1:
+        mid = int(len(numbers) / 2)
+
+        left = numbers[:mid]
+        right = numbers[mid:]
+
+        mergesort(left)
+        mergesort(right)
+
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(left) and j <len(right):
+            if left[i] < right[j]:
+                numbers[k] = left[i]
+                i += 1
+            else:
+                numbers[k] = right[j]
+                j += 1
+
+            k += 1
+
+        while i < len(left):
+            numbers[k] = left[i]
+            k += 1
+            i += 1
+
+        while j < len(right):
+            numbers[k] = right[j]
+            k += 1
+            j += 1
+
+
+if __name__ == '__main__':
+
+    numbers = [12, 8, 9, 24, 67, 6]
+    mergesort(numbers)
+
+    print(numbers)
